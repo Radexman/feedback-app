71aa9bb6ec352d7204d78de05d6e291ce505bb63
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-07-28 21:30:08 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
+import Header from "./components/Header";
+
 export default function App() {
-  const title = "Blog Post";
-  const body = "This is my blog post";
-  const comments = [
-    { id: 1, text: "Post One" },
-    { id: 2, text: "Post Two" },
-    { id: 3, text: "Post Three" },
-  ];
-
   return (
-    <div className="container mx-auto space-y-2 p-4 text-font-white">
-      <h1 className="text-center text-3xl font-semibold">
-        {title.toUpperCase()}
-      </h1>
-      <p className="text-center text-xl">{body}</p>
-
-      <div className="comments">
-        <h2 className="text-2xl">Comments ({comments.length})</h2>
-        <ul>
-          {comments.map((comment, index) => (
-            <li className="justify-left pl-3 text-sm" key={index}>
-              {comment.text}
-            </li>
-          ))}
-        </ul>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   );
 }
