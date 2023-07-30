8475cc4c4e9f8fa13d65735b6b83505490dc8bf4
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-07-30 18:48:37 +0200

    Prop Drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,13 @@
+import { FaTimes } from "react-icons/fa";
 import Card from "./shared/Card";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes onClick={() => handleDelete(item.id)} color="white" />
+      </button>
       <div className="text-dispaly">{item.text}</div>
     </Card>
   );
