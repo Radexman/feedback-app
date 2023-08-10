9e0bab91d1d60750771e2c74294a5433008f6649
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-10 17:19:17 +0200

    Events & Prop Drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,14 @@
+import { FaTimes as Xmark } from "react-icons/fa";
 import PropTypes from "prop-types";
-
 import Card from "./shared/Card";
 
-export default function FeedbackItem({ item }) {
+export default function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <Xmark color="white" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 import FeedbackItem from "./FeedbackItem";
 
-export default function FeedbackList({ feedback }) {
+export default function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
@@ -10,7 +10,7 @@ export default function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
