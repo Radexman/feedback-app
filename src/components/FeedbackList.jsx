cdcde277cdc4ff1864be5a0de61331220de113ee
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-17 15:27:44 +0200

    Fetch State

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import { motion, AnimatePresence } from "framer-motion";
-import PropTypes from "prop-types";
+import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
+import FeedbackContext from "../context/FeedbackContext";
 
-export default function FeedbackList({ feedback, handleDelete }) {
+export default function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
@@ -28,13 +31,3 @@ export default function FeedbackList({ feedback, handleDelete }) {
     </div>
   );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      rating: PropTypes.number.isRequired,
-      text: PropTypes.string,
-    }),
-  ),
-};
