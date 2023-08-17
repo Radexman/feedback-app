c3764e3c49bda730754485814a2f4442b63f579c
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-17 15:14:43 +0200

    Create Context & Provider

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
+import { FeedbackContext, FeedbackProvider } from "./context/FeedbackContext";
 import AboutIconLink from "./components/AboutIconLink";
 
 export default function App() {
@@ -24,30 +25,32 @@ export default function App() {
   };
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          ></Route>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
 
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
 
-        <AboutIconLink />
-      </div>
-    </Router>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -3,7 +3,7 @@ import { FaQuestion } from "react-icons/fa";
 
 export default function AboutIconLink() {
   return (
-    <div className="about-link">
+    <div className="about-link fixed bottom-2 right-2">
       <Link to="/about">
         <FaQuestion size={30} />
       </Link>
