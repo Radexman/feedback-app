fc40a9349cced692c7670716da48cff4e3474246
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-18 12:54:36 +0200

    Function Context

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,16 @@
 import { FaTimes as Xmark } from "react-icons/fa";
+import { useContext } from "react";
 import PropTypes from "prop-types";
 import Card from "./shared/Card";
+import FeedbackContext from "../context/FeedbackContext";
 
-export default function FeedbackItem({ item, handleDelete }) {
+export default function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
+      <button onClick={() => deleteFeedback(item.id)} className="close">
         <Xmark color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
