687993fdc3897b0e1ca170793329c9992b20bfdd
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-08-22 12:07:04 +0200

    Update & Delete From Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -30,14 +30,28 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
 
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const res = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Contetn-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await res.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)),
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item)),
     );
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
